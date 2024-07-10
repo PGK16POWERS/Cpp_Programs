e4961c45459daf705c07b60330521bb100299c1b
programmer Kartier <progrmmerKartier@gmail.com>
2024-07-10 16:02:02 +0200

    grimeyTouch

---
+++ DisplayTime/Time2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+// CREATE OBJECTS FOR ALL THE MONTHS OF THE YEAR AND DAYS ON THE WEEK
+string months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct", "Nov", "Dec"};
+string days[] = {"Mon","Tue","Wed","Thur","Fri","Sat", "Sun"};
+
+int main() {
+
+    time_t timer;
+    tm * timer;
+    const int BASE_YEAR = 1900;
+
+    cout << "The current time: " << [time->tm]
+    return 0;
+}
